37d38df4c30bd9010b7f70351d1c5c1e047b2a23
danish04dz <mohd.692003@gmail.com>
2025-07-14 16:04:28 +0530

    server cretaed

--- index.js
+++ index.js
@@ -3,15 +3,20 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-
 app.use(cors());
-
 app.use(express.json());
 
-// Main route 
 
+
+// Main route 
+app.get('/',(req,res)=>{
+   res.send('serrver is runing')
+})
 
 
 
 //create server Listining
+app.listen(4500,()=>{
+   console.log('server is running on http://localhost:4500');
+})
 
