bfbdb5d1d62fc9d238f5be023c636475060df8ff
danish04dz <mohd.692003@gmail.com>
2025-07-14 14:47:46 +0530

    server create

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("hello")
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+// Main route 
+
+
+
+
+//create server Listining
+
